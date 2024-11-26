46c967f45457e8d6b33935605bfe2d93a63a721f
CodCodingCode <nathanyan2008p@gmail.com>
2024-11-26 16:53:44 -0500

    Update test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11n.pt")
-
-model.train(
-    data="dataset.yaml",  # Path to your .yaml file created earlier
-    epochs=50,  # Number of epochs to train
-)
